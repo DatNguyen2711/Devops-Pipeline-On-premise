1c0e6e1b64800a5374422b460117ada0eae7763f
DatLaid <datlaid@gmail.com>
2024-07-19 00:27:24 +0700

    add health check docker

---
+++ healthcheck.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Load environment variables from .env file in the same directory
+source "$(dirname "$0")/.env"
+
+if /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1; then
+  exit 0
+else
+  exit 1
+fi
